32136f409aa1eb077e69069095435cabdf71ed18
LeuAlmeida <leo@webid.net.br>
2019-11-27 16:18:51 -0300

    Correção na data do timer

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date("Sep 30, 2020 00:00:00").getTime(),
+let countDown = new Date("Dec 08, 2019 00:00:00").getTime(),
   x = setInterval(function() {
     let now = new Date().getTime(),
       distance = countDown - now;
