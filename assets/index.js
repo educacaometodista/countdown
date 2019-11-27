f9626adb97c1ea552548737a9dd13ca9fa66d77c
LeuAlmeida <leo@webid.net.br>
2019-11-27 14:05:31 -0300

    Melhorias no script de countdown

--- assets/index.js
+++ assets/index.js
@@ -1,36 +1,27 @@
-// Set the date we're counting down to
-var countDownDate = new Date("May 5, 2020 12:00:00").getTime();
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-  // Get todays date and time
-  var now = new Date().getTime();
+let countDown = new Date("Sep 30, 2020 00:00:00").getTime(),
+  x = setInterval(function() {
+    let now = new Date().getTime(),
+      distance = countDown - now;
 
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+    (document.getElementById("days").innerText = Math.floor(distance / day)),
+      (document.getElementById("hours").innerText = Math.floor(
+        (distance % day) / hour
+      )),
+      (document.getElementById("minutes").innerText = Math.floor(
+        (distance % hour) / minute
+      )),
+      (document.getElementById("seconds").innerText = Math.floor(
+        (distance % minute) / second
+      ));
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("vestibularCountdown").innerHTML =
-    'Faltam: ' +
-    days +
-    (days > 0 ? ' dias ' : ' dia ') + 
-    hours +
-    (hours > 0 ? ' horas ' : ' hora ') +
-    minutes +
-    (minutes > 0 ? ' minutos ' : ' minuto ') +
-    seconds +
-    (seconds > 0 ? ' segundos ' : ' segundo ');
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("vestibularCountdown").innerHTML =
-      "JÁ ESTÁ ACONTECENDO!";
-  }
-}, 1000);
+    //do something later when date is reached
+    //if (distance < 0) {
+    //  clearInterval(x);
+    //  '';
+    //}
+  }, second);
